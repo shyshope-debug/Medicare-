f06f05c53e9c836b225b8d67a2b9ff4b7dd54a47
shyshope-debug <shyshope@gmail.com>
2026-07-04 21:16:24 +0300

    Add authentication URLs and update home path

--- medicare/urls.py
+++ medicare/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pharmacy.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # This shows "Medicare is Live!" at your root URL
+    path('accounts/', include('django.contrib.auth.urls')),  # adds login/logout/password reset
+    path('', home, name='home'),
 ]
