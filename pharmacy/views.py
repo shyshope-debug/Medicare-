09b12f4977172488231a311105747ff9e6d88566
shyshope-debug <shyshope@gmail.com>
2026-07-04 21:36:37 +0300

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Medicare is Live!")
+    if request.user.is_authenticated:
+        return HttpResponse(f"Welcome to Medicare, {request.user.username}! <a href='/accounts/logout/'>Logout</a>")
+    else:
+        return HttpResponse("Medicare is Live! <a href='/accounts/login/'>Login</a>")
