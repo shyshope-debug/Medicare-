94d516178916afd150c960a667091d88bd759552
shyshope-debug <shyshope@gmail.com>
2026-07-04 21:54:32 +0300

    Update superuser.py

--- superuser.py
+++ superuser.py
@@ -1,13 +1,1 @@
-import os
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicare.settings')
-django.setup()
-
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
-    print('Superuser created')
-else:
-    print('Superuser already exists')
