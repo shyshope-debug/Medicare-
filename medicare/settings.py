c9704496919680276a5b0554aa16c7299b62597b
shyshope-debug <shyshope@gmail.com>
2026-07-05 00:18:50 +0300

    Update settings.py

--- medicare/settings.py
+++ medicare/settings.py
@@ -53,10 +53,10 @@ WSGI_APPLICATION = 'medicare.wsgi.application'  # lowercase m
 
 # DATABASE - USE SQLITE FOR NOW TO STOP THE HANGING
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
