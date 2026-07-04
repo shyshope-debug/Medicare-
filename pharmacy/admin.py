d6319de630802159b9d8422ffe20f28e846d909f
shyshope-debug <shyshope@gmail.com>
2026-07-04 18:45:06 +0300

    Update CustomUserAdmin to exclude 'role' field
    Removed 'role' field from CustomUserAdmin display.

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -3,9 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Role', {'fields': ('role',)}),
-    )
-    list_display = ['username', 'email', 'role', 'is_staff']
+    list_display = ['username', 'email', 'is_staff']
 
 admin.site.register(User, CustomUserAdmin)
