1d884d0061872ce930b0e5d1c0e4e242fc82d316
shyshope-debug <shyshope@gmail.com>
2026-07-04 18:26:38 +0300

    Reformat User model fields for consistency

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 class User(AbstractUser):
-       groups = models.ManyToManyField(
-           'auth.Group',
-           verbose_name='groups',
-           blank=True,
-           help_text='The groups this user belongs to.',
-           related_name="pharmacy_user_set",
-           related_query_name="user",
-       )
-      user_permissions = models.ManyToManyField(
-           'auth.Permission',
-           verbose_name='user permissions',
-           blank=True,
-           help_text='Specific permissions for this user.',
-           related_name="pharmacy_user_set",
-           related_query_name="user",
-       )
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name="pharmacy_user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="pharmacy_user_set",
+        related_query_name="user",
+    )
