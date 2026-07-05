e47702d81591791cda5716a865f1c49f0612eadb
shyshope-debug <shyshope@gmail.com>
2026-07-05 10:41:48 +0300

    Update settings.py

--- medicare/settings.py
+++ medicare/settings.py
@@ -3,30 +3,9 @@ import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  # ADD THIS LINE
 import os
 
 # For Render
 ALLOWED_HOSTS = ['medicare-lnan.onrender.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://medicare-lnan.onrender.com']
-
-# Static files for admin CSS
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-SECRET_KEY = 'django-insecure-change-this-in-production'
-
-# Turn this False once everything works
-DEBUG = os.environ.get('DEBUG', 'False')
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'pharmacy',  # your app
-]
-
-# ... rest of your file stays the same
