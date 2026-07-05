2d6244cd482203a4b8b11740504de462c90de749
shyshope-debug <shyshope@gmail.com>
2026-07-05 08:42:31 +0300

    Update urls.py

--- medicare/urls.py
+++ medicare/urls.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Medicare is online. Go to /admin to login")
+
+urlpatterns = [
+    path('', home),
+    path('admin/', admin.site.urls),
+]
