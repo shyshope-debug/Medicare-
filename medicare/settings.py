b2aed2abbeb4632999ab35a7499ee9836918fba5
shyshope-debug <shyshope@gmail.com>
2026-07-05 10:51:58 +0300

    Add middleware and templates configuration

--- medicare/settings.py
+++ medicare/settings.py
@@ -28,3 +28,31 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pharmacy',  # your app
 ]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # For static files
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+ROOT_URLCONF = 'medicare.urls'
+WSGI_APPLICATION = 'medicare.wsgi.application'
