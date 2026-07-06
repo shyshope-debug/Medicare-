ffb7b718f391716a7d4e9b8cfe08dd39282af9a6
shyshope-debug <shyshope@gmail.com>
2026-07-06 18:16:40 +0300

    Update urls.py

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('order/<int:medicine_id>/', views.order_medicine, name='order_medicine'),
+    path('webhook/', views.whatsapp_webhook, name='webhook'),
 ]
