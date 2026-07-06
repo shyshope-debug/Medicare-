00acfcf17ad43f0c2f6cf9f25341d193b1f101b9
shyshope-debug <shyshope@gmail.com>
2026-07-06 04:28:01 +0300

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Medicine
 
 def home(request):
-    medicines = Medicine.objects.all()  # Changed this line
+    medicines = Medicine.objects.all()  # Removed .filter(available=True)
     return render(request, 'pharmacy/home.html', {'medicines': medicines})
