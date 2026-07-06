338ec03813e5fd3576ac1c240829a257a5db4387
shyshop <shyshope@gmail.com>
2026-07-06 13:49:47 +0300

    register Order in admin

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Medicine
+from .models import User, Medicine, Order
 
 admin.site.register(User)
 admin.site.register(Medicine)
+admin.site.register(Order)
